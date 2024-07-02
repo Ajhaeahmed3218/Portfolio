c623099763200395cccfe2d5152edf637c6f69ba
Ajharahmed3218 <nuhash3218@gmail.com>
2024-07-03 01:44:10 +0600

    Add : Banner section Part 1 Added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,3 +1,6 @@
+import Data from "./Data";
+import Social from "./Social";
+import "./home.css"
 
 
 const Home = () => {
@@ -6,7 +9,11 @@ const Home = () => {
             <section className="home section" id="home" ></section>
             <div className="home__container container grid">
                 <div className="home__content grid">
-                    
+                    <Social />
+
+                    <div className="home__img"></div>
+
+                    <Data /> 
                 </div>
             </div>
         </div>
--- src/Components/Home/Social.jsx
+++ src/Components/Home/Social.jsx
@@ -7,9 +7,13 @@ const Social = () => {
                 <i className="uil uil-instagram"></i>
             </a>
 
-            <a href="" className="home__social-icon" target='_blank'></a>
+            <a href="https://github.com/Ajhaeahmed3218" className="home__social-icon" target='_blank'>
+                <i className="uil uil-github-alt"></i>
+            </a>
 
-            <a href="" className="home__social-icon" target='_blank'></a>
+            <a href="https://www.facebook.com/profile.php?id=100071592883404" className="home__social-icon" target='_blank'>
+                <i className="uil uil-facebook"></i>
+            </a>
         </div>
     );
 };
