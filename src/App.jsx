3e13c5f1f2b94afcd7360acdba38465e2e21d05c
Ajharahmed3218 <nuhash3218@gmail.com>
2024-07-03 09:02:55 +0600

    Add - Skills section is Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import About from './Components/About/About'
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
+import Skills from './Components/Skills/Skills'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <main className='main'>
         <Home />
         <About />
+        <Skills />
       </main>
 
     </>
